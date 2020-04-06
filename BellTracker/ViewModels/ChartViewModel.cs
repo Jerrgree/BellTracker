1b57de8a48edb67d6ad13107e0567c6569df07b9
Jerrgree <jeremyrgreenburg@gmail.com>
2020-04-06 18:45:58 -0500

    Integrate Index page to view model and data store

--- BellTracker/ViewModels/ChartViewModel.cs
+++ BellTracker/ViewModels/ChartViewModel.cs
@@ -1,8 +1,24 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Common;
+using Common.Models;
+using Domain;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
 
 namespace BellTracker.ViewModels
 {
     public class ChartViewModel : ComponentBase
     {
+        [Inject]
+        public IBellDataStore BellDataStore { get; set; }
+
+        protected Week CurrentWeek { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var currentDate = new ParsedDate(DateTime.Now);
+            CurrentWeek = await BellDataStore.GetOrCreateWeek(currentDate.Year, currentDate.Week);
+            await base.OnInitializedAsync();
+        }
     }
 }
--- Common/Models/ParsedDate.cs
+++ Common/Models/ParsedDate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Common.Models
@@ -8,14 +9,18 @@ namespace Common.Models
     {
         public DayOfWeek DayOfWeek { get; private set; }
         public string Year { get; private set; }
-
         public bool IsMorning { get; private set; }
+        public int Week { get; private set; }
 
         public ParsedDate(DateTime dateTime)
         {
             DayOfWeek = dateTime.DayOfWeek;
             Year = dateTime.Year.ToString();
             IsMorning = dateTime.TimeOfDay < TimeSpan.FromHours(12);
+            Week = CultureInfo
+                .CurrentCulture
+                .Calendar
+                .GetWeekOfYear(dateTime, CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
         }
     }
 }
