93c2cd607445e493fcfc7cdfe6efaf38f74069e2
Jerrgree <jeremyrgreenburg@gmail.com>
2020-04-06 18:19:59 -0500

    Add ParseDate model

---
+++ Common/Models/ParsedDate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Models
+{
+    public class ParsedDate
+    {
+        public DayOfWeek DayOfWeek { get; private set; }
+        public string Year { get; private set; }
+
+        public bool IsMorning { get; private set; }
+
+        public ParsedDate(DateTime dateTime)
+        {
+            DayOfWeek = dateTime.DayOfWeek;
+            Year = dateTime.Year.ToString();
+            IsMorning = dateTime.TimeOfDay < TimeSpan.FromHours(12);
+        }
+    }
+}
